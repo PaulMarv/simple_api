bf7c0789286204ad8977eebb72ca72f7166d2040
PaulMarv <mpsalako@gmail.com>
2025-03-10 12:18:42 +0100

    fetch single amd edit product

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,13 +1,33 @@
 import Product from "../models/product.model.js";
 
+
+//GET /api/products
 export const getProducts = async (req, res, next) => {
   try {
       const products = await Product.find({});
-      res.json(products);
+      res.status(200).json(products);
   }catch(error){
       next(error);
   }
 };
+
+//GET /api/products/:id
+export const getProductById = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if(!product){
+            const error = new Error('The product is not available')
+            error.status = 404;
+            return next(error)
+        }
+        res.status(200).json(product)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+//POST /api/products
 export const postProduct = async (req, res, next) => {
   try {
     if (
@@ -25,3 +45,19 @@ export const postProduct = async (req, res, next) => {
     next(error);
   }
 };
+
+//PUT /api/products/:id
+export const updateProduct = async (req, res, next ) =>{
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body);
+        if(!product){
+            const error = new Error('Product not found');
+            error.status = 404;
+            return next(error);
+        }
+        const updatedProduct = await Product.findById(req.params.id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        next(error);    
+    }
+}
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getProducts, postProduct } from '../controller/productsController.js';
+import { getProductById, getProducts, postProduct, updateProduct } from '../controller/productsController.js';
 
 const router = express.Router();
 
 router.get("/", getProducts);
+router.get("/:id", getProductById)
 router.post("/", postProduct);
+router.put("/:id", updateProduct);
 
 export default router;
